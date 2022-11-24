8d99d6236b82aa81c46055d1fe820bc300270741
KamalEmon <u1304013@student.cuet.ac.bd>
2022-11-24 19:25:41 +0600

    Database connected and users collection created

--- index.js
+++ index.js
@@ -3,13 +3,47 @@ const cors = require("cors");
 
 require("dotenv").config();
 const jwt = require("jsonwebtoken");
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ph4ajav.mongodb.net/?retryWrites=true&w=majority`;
 
+
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+//Database Connection
+async function dbConnect() {
+    try {
+      client.connect();
+    } catch (err) {
+      console.log(err.name);
+    }
+  }
+  dbConnect();
+
+// creating Database collection 
+const usersCollection = client.db('antique-market').collection('users');
+
+
+
+
+// app.put('/users',async(req,res)=>{
+//     const user = req.body;
+//     const filter = {email: req.body.email}
+//     const options = {upsert: true}
+//     const updateDoc = {
+//         $set: {
+                
+//         }
+//     }
+//     const result = await 
+
+// })
 
 app.get("/", (req, res) => {
   res.send(`Welcome to Antique Market Server`);
