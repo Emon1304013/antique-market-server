dc1ab7d37a64f08bf4f3cd68b2afdf4be4fc09f2
KamalEmon <u1304013@student.cuet.ac.bd>
2022-11-25 01:12:48 +0600

    Products api created to post product data

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 
 require("dotenv").config();
 const jwt = require("jsonwebtoken");
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -12,38 +12,59 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ph4ajav.mongodb.net/?retryWrites=true&w=majority`;
 
-
-
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
 //Database Connection
 async function dbConnect() {
-    try {
-      client.connect();
-    } catch (err) {
-      console.log(err.name);
-    }
+  try {
+    client.connect();
+  } catch (err) {
+    console.log(err.name);
   }
-  dbConnect();
+}
+dbConnect();
 
-// creating Database collection 
-const usersCollection = client.db('antique-market').collection('users');
+// creating Database collection
+const usersCollection = client.db("antique-market").collection("users");
+const productsCollection = client.db("antique-market").collection("products");
 
+// save user email and generate jwt
+app.put("/user/:email", async (req, res) => {
+  try {
+    const email = req.params.email;
+    console.log(email);
+    const user = req.body;
+    const filter = { email: email };
+    const options = { upsert: true };
+    const updateDoc = {
+      $set: user,
+    };
+    const result = await usersCollection.updateOne(filter, updateDoc, options);
+    console.log(result);
 
+    const token = jwt.sign(user, process.env.JWT_SECRET, { expiresIn: "1d" });
 
+    res.send({ result, token });
+  } catch (err) {
+    console.log(err);
+  }
+});
 
-// app.put('/users',async(req,res)=>{
-//     const user = req.body;
-//     const filter = {email: req.body.email}
-//     const options = {upsert: true}
-//     const updateDoc = {
-//         $set: {
-                
-//         }
-//     }
-//     const result = await 
-
-// })
+//add product to database
+app.post("/products", async(req,res) => {
+  try {
+    const product = req.body;
+    console.log(product);
+    const result = await productsCollection.insertOne(product);
+    res.send(result);
+  } catch (err) {
+    res.send(err);
+  }
+});
 
 app.get("/", (req, res) => {
   res.send(`Welcome to Antique Market Server`);
